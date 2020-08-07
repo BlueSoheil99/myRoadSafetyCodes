80a34d96b45c05959a3e796d52bfade8d97d566a
BlueSoheil99 <Soheil.k.78@gmail.com>
2020-08-07 21:36:50 +0430

    skewness is fine now. kurtosis was wrong! for next, i'm gonna make datas cleaner in dayCleaner.py

--- VolumeAndSpeed.py
+++ VolumeAndSpeed.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import skew
 import os
+
 
 os.chdir(r'D:\Educational\proje\data')
 input_address = r'traffic\gilan95\543451-95-AstanehToLahijan.xlsx'
 routeCode = '543451'
-output_address = input_address[:-5] + '-pro.xlsx'
-Traffic = pd.read_excel(input_address)
 
+
+output_address = input_address[:-5] + '-proff.xlsx'
+Traffic = pd.read_excel(input_address)
 ########################
 ########################
 Start = Traffic['start'].tolist()
@@ -31,6 +34,8 @@ SpeedVariation = []
 DailyTraffic = []
 DT_variation = []
 TruckPercentages = []
+SpeedSkewness = []
+DT_Skewness = []
 ########################
 ########################
 currentDate = ''
@@ -43,7 +48,6 @@ Code = []
 ########################
 days_with_missing_hours = []
 days_with_missing_minutes = []
-
 
 # todo : don't forget to fix missing values
 ########################
@@ -60,17 +64,19 @@ def sum_of_heavy_vehicles(row):
 
 
 def handle_previous_date():
+    Code.append(routeCode)
     Dates.append(currentDate)
     DailyTraffic.append(np.sum(totalVehicles))
     TruckPercentages.append(round(np.sum(totalHeavyVehicles) / np.sum(totalVehicles) * 100, 2))
     DT_variation.append(round(np.var(totalVehicles)))
     AverageSpeeds.append(round(np.mean(speeds), 2))
     SpeedVariation.append(round(np.var(speeds), 2))
-    Code.append(routeCode)
+    # TODO
+    SpeedSkewness.append(skew(speeds))
+    DT_Skewness.append(skew(totalVehicles))
 
 
 def add_info_to_daily_details(rowNumber, selectedDate):
-    # TODO: calculate kurtosis
     totalVehicles.append(TotalEstimated[rowNumber])
     totalHeavyVehicles.append(sum_of_heavy_vehicles(rowNumber))
     speeds.append(Avg_s[rowNumber])
@@ -92,7 +98,7 @@ for i in range(len(Start)):
             if hours != 24:
                 days_with_missing_hours.append(currentDate)
             handle_previous_date()
-            # clear_lists()
+            # clear_lists
             currentDate = ''
             hours = 0
             totalVehicles = []
@@ -107,7 +113,7 @@ print("missing minutes in:\n", days_with_missing_minutes)
 
 data = pd.DataFrame(
     {'Code': Code, 'Date': Dates, 'DailyTraffic': DailyTraffic, 'DT_variation': DT_variation,
-     'TruckPercentages': TruckPercentages,
-     'AverageSpeeds': AverageSpeeds, 'SpeedVariation': SpeedVariation}) # TODO: also add kurtosis for volume and speed
+     'DT_skewness': DT_Skewness, 'TruckPercentage': TruckPercentages, 'AverageSpeed': AverageSpeeds,
+     'SpeedVariation': SpeedVariation, 'SpeedSkewness': SpeedSkewness})
 
 data.to_excel(output_address)
